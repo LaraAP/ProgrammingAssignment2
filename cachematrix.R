4904043ab702a137b16a4e7d6b876a6aa478843a
LaraAP <85121629+LaraAP@users.noreply.github.com>
2021-06-01 15:43:30 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#makeCacheMatrix creates a list of functions
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  } #create the matrix
+  get <- function() x #get matrix value
+  setinv <- function(inverse) i <<- inverse  #invert  matrix
+  getinv <- function() i #get invert matrix from cache
+  list(set = set, get = get, setinv = setinv, getinv = getinv) #list of created functions
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve - calculate inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv() #try to get inv matrix stored in cache
+  if (!is.null(i)) {
+    message("cached data")
+    return(i)
+  } # return inverted matrix from cache. if the matrix does not exists -> create a matrix
+  ma <- x$get() #create matrix if it does not exist
+  i <- solve(ma, ...)
+  x$setinv(i)
+  i
 }
+
+
+#### TESTING ####
+M1 <- matrix(c(1,2,3,4),2,2)
+M2 <- makeCacheMatrix(M1)
+cacheSolve(M2)
